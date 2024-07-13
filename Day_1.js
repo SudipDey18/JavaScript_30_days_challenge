8ee118a2a4b82f3ff22117fcaa8f603128e53ea6
Sudip Dey <sdey7584@gmail.com>
2024-07-13 23:44:39 +0530

    Activity 2 and 3

--- Day_1.js
+++ Day_1.js
@@ -9,6 +9,27 @@ console.log(num);
 // Task 2: Declare a variable using let, assign it a string, and log the value to the console.
 // Ans
 let newstring = "Sudip Dey";
-console.log(newstring);
+console.log(newstrinewstringng);
 
+// Activity 2: Constant Declaration
+// Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+// Ans
+const newboolean = true;
+console.console.log(newboolean);
+
+// Activity 3: Data Types
+// Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator.
+let ph_number = 354;
+let name = "Sudip Dey"
+let cart_value = [200,600,400,150];
+let my_info = {
+    name : "Sudip Dey ",
+    age : 18,
+}
+is_login = true;
+let salary = undefined;
+console.log(typeof ph_number);
+console.log(typeof name);
+console .log(typeof cart_value);
+console.log(typeof my_info);
 
