2e9bb77cc6c50365ecac7545953c2957f3499f13
Sudip Dey <sdey7584@gmail.com>
2024-07-13 23:17:14 +0530

    Activity 1

---
+++ Day_1.js
@@ -0,0 +1,14 @@
+// Day 1: Variables and Data Types
+
+// Activity 1:  Variable Declaration
+// Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+// Ans
+var num = 18;
+console.log(num);
+
+// Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+// Ans
+let newstring = "Sudip Dey";
+console.log(newstring);
+
+
