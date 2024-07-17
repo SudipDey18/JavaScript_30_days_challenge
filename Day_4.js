997687ef3e090921ed7f2038c684d664a89ae584
Sudip Dey <sdey7584@gmail.com>
2024-07-17 14:43:49 +0530

    day 4 update

--- Day_4.js
+++ Day_4.js
@@ -54,3 +54,32 @@ console.log(factorial);
 **
 ***
 ****
+*****
+*/
+// Ans
+for (let i = 1; i <= 5; i++){
+    let temp;
+    for (let j = 1; j <= i; i++){
+        temp += '*';
+    }
+    console.log(temp);
+}
+
+// Activity 5: Loop Control Statements
+// Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the 'continue' statement.
+// Ans
+for (let i = 1; i <= 10; i++){
+    if(i === 5){
+        continue;
+    }
+    console.log(i);
+}
+
+// Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the 'break' statement.
+// Ans
+for (let i = 1; i <= 10; i++){
+    if (i===7){
+        break;
+    }
+    console.log(i);
+}
